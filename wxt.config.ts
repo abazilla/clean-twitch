14f27e2d9a66d9fad32b6a797b7cd3cf8a520acc
pat <patrick.aba@gmail.com>
2025-11-07 17:11:14 +1100

    remove unused manifest permission

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from "@tailwindcss/vite"
-import { defineConfig, type WxtViteConfig } from "wxt"
 import checker from "vite-plugin-checker"
+import { defineConfig, type WxtViteConfig } from "wxt"
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
@@ -9,11 +9,11 @@ export default defineConfig({
 		({
 			plugins: [
 				tailwindcss(),
-				checker({ 
+				checker({
 					typescript: true,
 					enableBuild: false,
-					overlay: { initialIsOpen: false }
-				})
+					overlay: { initialIsOpen: false },
+				}),
 			],
 		}) as WxtViteConfig,
 	manifest: {
@@ -21,12 +21,12 @@ export default defineConfig({
 		// 	default_popup: "/entrypoints/popup/index.html",
 		// },
 		host_permissions: ["https://*.twitch.tv/*"],
-		permissions: ["tabs", "scripting", "storage"],
+		permissions: ["tabs", "storage"],
 		browser_specific_settings: {
 			gecko: {
-				id: "{8f2e5b0a-9d22-4a63-a9a0-b924e19fb1e2}"
-			}
-		}
+				id: "{8f2e5b0a-9d22-4a63-a9a0-b924e19fb1e2}",
+			},
+		},
 	},
 	webExt: {
 		disabled: true,
